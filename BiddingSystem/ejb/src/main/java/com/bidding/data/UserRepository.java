8c1fbcda91dacc3fc9eeaba6eef979176ca6d42b
Kavimal Wijewardana <kavimal@thinkcube.com>
2014-02-11 22:57:51 +0530

    Add new methods to UserRepository from UserManagement

--- BiddingSystem/ejb/src/main/java/com/bidding/data/UserRepository.java
+++ BiddingSystem/ejb/src/main/java/com/bidding/data/UserRepository.java
@@ -10,6 +10,8 @@ import javax.persistence.criteria.Root;
 
 import com.bidding.model.User;
 
+import java.util.List;
+
 @Stateless
 public class UserRepository {
 	
@@ -29,7 +31,27 @@ public class UserRepository {
         criteria.select(user).where(cb.equal(user.get("email"), email));
         return em.createQuery(criteria).getSingleResult();
     }
-    
+
+    public User getUserByEmail(String email) {
+        User user = null;
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+        Root<User> getUser = criteriaQuery.from(User.class);
+        criteriaQuery.select(getUser).where(criteriaBuilder.equal(getUser.get("email"), email));
+        user = em.createQuery(criteriaQuery).getSingleResult();
+        return user;
+    }
+
+    public List<User> getUsersByUserName(String firstName) {
+        List<User> users;
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+        Root<User> getUser = criteriaQuery.from(User.class);
+        criteriaQuery.select(getUser).where(criteriaBuilder.equal(getUser.get("userName"), firstName));
+        users = em.createQuery(criteriaQuery).getResultList();
+        return users;
+    }
+
     public void saveUser(User user){
     	em.persist(user);
     }
